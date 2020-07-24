dbdeb206ae202e4ff2a391ec4d3bd936dc41cf94
Luka Latkovic <latkovic.luka@gmail.com>
2020-07-25 00:47:54 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lola_.NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
